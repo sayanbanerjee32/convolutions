cf0a378b3f00b4dddbdc3243c957d0df3e60fff9
sayanbanerjee32 <sayanbanerjee32@gmail.com>
2024-03-26 19:02:20 +0530

    need to split utils later

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from tqdm import tqdm
 
 import torch
-from torchvision import transforms
+from torchvision import transforms, datasets
 
 import matplotlib.pyplot as plt
 import pandas as  pd
@@ -215,3 +215,15 @@ def inverse_normalize(tensor, mean, std):
                 )
     return inv_normalize(tensor)
     
+class Cifar10Dataset(datasets.CIFAR10):
+    def __init__(self, root = './data', train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+
+        return image, label
