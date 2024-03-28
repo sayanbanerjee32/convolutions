f6dda776df1f024d4f7124384a1c753c0c4bec5c
sayanbanerjee32 <sayanbanerjee32@gmail.com>
2024-03-28 09:26:33 +0530

    ## Depthwise - 460,384 - 49% - 71% - 20 epochs

---
+++ model_1.py
@@ -0,0 +1,238 @@
+## Depthwise - 460,384 - 49% - 71% - 20 epochs
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, dropout_value = 0.1):
+        super(Net, self).__init__()
+        # # Input Block / CONVOLUTION BLOCK 1
+        self.C1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) #input size: 32 x 32 x 3, output size: 32 x 32 x 32, receptive field: 1 + (3-1) * 1 = 3
+
+
+        self.C2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) #input size: 32 x 32 x 32, output size: 32 x 32 x 64, receptive field: 3 + (3-1) * 1 = 5
+
+        # ## Strided convolution
+        # self.SC1 = nn.Sequential(
+        #     nn.Conv2d(in_channels=64, out_channels=64,
+        #               kernel_size=(3, 3), padding=1, stride = 2, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(64),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 32 x 32 x n*2, output size: 24 x 24 x n*4, receptive field: 3 + (3-1) * 1 = 5
+        # #Lout =  (Lin + 2 * padding - dilation * (kernel - 1) - 1) / stride + 1
+        # # ((32 + 2 * 1 - 1 * (3 - 1) -1) / 2) + 1 = 16
+
+        ## dilated convolution
+        self.SC1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64,
+                      kernel_size=(3, 3), padding=1, dilation = 8, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) #input size: 32 x 32 x n*2, output size: 24 x 24 x n*4, receptive field: 3 + (3-1) * 1 = 5
+        # ((32 + 2 * 1 - 8 * (3 - 1) -1) / 1) + 1 = 25
+
+        # TRANSITION BLOCK 1
+        self.t1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=32,
+                      kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) #input size: 17 x 17 x n*4, output size: 17 x 17 x n, receptive field: 6 + (1-1)*2 = 6
+
+
+        # CONVOLUTION BLOCK 2
+        self.C3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n, output size: 16 x 16 x n*2, receptive field: 6 + (3-1) * 2 = 10
+
+        self.C4 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n*2, output size: 16 x 16 x n*4, receptive field: 10 + (3-1) * 2 = 14
+
+        # self.SC2 = nn.Sequential(
+        #     nn.Conv2d(in_channels=128, out_channels=128,
+        #               kernel_size=(3, 3), padding=1, stride = 2, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(128),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 16 x 16 x n*4, output size: 16 x 16 x n*4, receptive field: 14 + (3-1) * 2 = 18
+
+        self.SC2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128,
+                      kernel_size=(3, 3), padding=1, dilation = 4, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n*4, output size: 16 x 16 x n*4, receptive field: 14 + (3-1) * 2 = 18
+        # ((18 + 2 * 1 - 4 * (3 - 1) -1) / 1) + 1 = 12
+        # TRANSITION BLOCK 2
+        self.t2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=32,
+                      kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n*4, output size: 16 x 16 x n, receptive field: 18 + (1-1)*2 = 18
+
+        # CONVOLUTION BLOCK 3
+        # self.C5 = nn.Sequential(
+        #     nn.Conv2d(in_channels=32, out_channels=64,
+        #               kernel_size=(3, 3), padding=1, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(64),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 16 x 16 x n, output size: 16 x 16 x n*2, receptive field: 6 + (3-1) * 2 = 10
+
+        # self.C6 = nn.Sequential(
+        #     nn.Conv2d(in_channels=64, out_channels=128,
+        #               kernel_size=(3, 3), padding=1, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(128),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 16 x 16 x n*2, output size: 16 x 16 x n*4, receptive field: 10 + (3-1) * 2 = 14
+        self.C5 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, groups = 32,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n, output size: 16 x 16 x n*2, receptive field: 6 + (3-1) * 2 = 10
+
+        self.C6 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, groups = 64,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n*2, output size: 16 x 16 x n*4, receptive field: 10 + (3-1) * 2 = 14
+
+        # self.SC3 = nn.Sequential(
+        #     nn.Conv2d(in_channels=128, out_channels=128,
+        #               kernel_size=(3, 3), padding=1, stride = 2, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(128),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 16 x 16 x n*4, output size: 16 x 16 x n*4, receptive field: 14 + (3-1) * 2 = 18
+        self.SC3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128,
+                      kernel_size=(3, 3), padding=1, dilation = 2, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n*4, output size: 16 x 16 x n*4, receptive field: 14 + (3-1) * 2 = 18
+        # ((12 + 2 * 1 - 4 * (3 - 1) -1) / 1) + 1 = 11
+        # TRANSITION BLOCK 3
+        self.t3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=32,
+                      kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) #input size: 16 x 16 x n*4, output size: 16 x 16 x n, receptive field: 18 + (1-1)*2 = 18
+
+        # CONVOLUTION BLOCK 4
+        # self.C7 = nn.Sequential(
+        #     nn.Conv2d(in_channels=32, out_channels=64,
+        #               kernel_size=(3, 3), padding=1, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(64),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 8 x 8 x n, output size: 8 x 8 x n*2, receptive field: 20 + (3-1) * 4 = 28
+
+        # self.C8 = nn.Sequential(
+        #     nn.Conv2d(in_channels=64, out_channels=128,
+        #               kernel_size=(3, 3), padding=1, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(128),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 8 x 8 x n*2, output size: 8 x 8 x n*4, receptive field: 28 + (3-1) * 4 = 36
+
+        self.C7 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, groups = 32,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value)
+        ) #input size: 8 x 8 x n, output size: 8 x 8 x n*2, receptive field: 20 + (3-1) * 4 = 28
+
+        self.C8 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, groups = 64,
+                      kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value)
+        ) #input size: 8 x 8 x n*2, output size: 8 x 8 x n*4, receptive field: 28 + (3-1) * 4 = 36
+
+        # self.C10 = nn.Sequential(
+        #     nn.Conv2d(in_channels=n*4, out_channels=n*4,
+        #               kernel_size=(3, 3), padding=1, bias=False),
+        #     nn.ReLU(),
+        #     nn.BatchNorm2d(n*4),
+        #     nn.Dropout(dropout_value)
+        # ) #input size: 8 x 8 x n*4, output size: 8 x 8 x n*4, receptive field: 36 + (3-1) * 4 = 44
+
+        # OUTPUT BLOCK
+        self.GAP = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1))
+        ) #input size: 8 x 8 x n*4, output size: 1 x 1 x n*4, receptive field: 44
+
+        self.c11 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=10,
+                      kernel_size=(1, 1), padding=0, bias=False),
+        ) #input size: 1 x 1 x n*4, output size: 1 x 1 x 10, receptive field: 44 + (1-1) * 4 =44
+
+
+
+    def forward(self, x):
+        x = self.C1(x)
+        x = self.C2(x)
+
+        x = self.SC1(x)
+        x = self.t1(x)
+
+        x = self.C3(x)
+        x = self.C4(x)
+
+        x = self.SC2(x)
+        x = self.t2(x)
+
+        x = self.C5(x)
+        x = self.C6(x)
+
+        x = self.SC3(x)
+        x = self.t3(x)
+
+        x = self.C7(x)
+        x = self.C8(x)
+
+        x = self.GAP(x)
+        x = self.c11(x)
+
+        x = x.squeeze()
+
+        return F.log_softmax(x, dim=-1)
